a6dc3c9dc5f88fb17a590ce7995346a5b33ba9e1
kleber chavez <kleberenriqueos@gmail.com>
2023-07-02 21:39:01 -0500

    css para navBar

--- usabilidad/src/componets/navBar.jsx
+++ usabilidad/src/componets/navBar.jsx
@@ -3,17 +3,15 @@ import React from 'react';
 const NavBar = () => {
     return (
         <div className='navBar'>
-        <nav> 
             <div className='logo'> 
                 <img src="src\assets\iconos\onlineShop.png" alt="" />
                 <h1>WEB STORE</h1>
             </div>
             <div className='navBar_icon'>
                 <input type="text" placeholder='Buscar'/>
-                <i class="fa-sharp fa-light fa-cart-shopping"></i>
+                <i className="fa-sharp fa-solid fa-cart-shopping"></i>
                 <i className="fa-regular fa-user"></i>
             </div>
-        </nav>
             
         </div>
     );
