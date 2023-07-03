54f9ee0f76314e228bd06829d0023c24c2abf41b
kleber chavez <kleberenriqueos@gmail.com>
2023-07-02 20:15:21 -0500

    nueva info

---
+++ usabilidad/src/componets/navBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const navBar = () => {
+    return (
+        <div>
+        <nav>
+            <img src="" alt="" />
+        </nav>
+            
+        </div>
+    );
+};
+
+export default navBar;
