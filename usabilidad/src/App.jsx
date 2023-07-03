ca304981f065aff2925135bb758e20e7b1baa8a3
kleber chavez <kleberenriqueos@gmail.com>
2023-07-02 21:03:12 -0500

    navBar

--- usabilidad/src/App.jsx
+++ usabilidad/src/App.jsx
@@ -1,32 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './componets/navBar'
 
 function App() {
-  //variables dinamicas[primer nombre, nombre final]para que se renderise
-  const [contador,setContador] = useState(0);//dentro de usestate hay que poner el tipo de dato o objeto
-  const [tex, setTex] = useState('hola')
-  //variables estaticas para que se renderise
-  const prueba = 20;
-
-  //funcion flecha 
-  const contador_ = () =>{
-    setContador(contador + 1)
-    setTex('adios')
-  }  
-  //controlar un estado de vectadero y falso
+  
+   
+ 
   
 
   return (
     // codigo <html>
     <>
-      <button onClick={()=>contador_ ()}>
-        incrementar {contador}
-      </button>
-      <p >
-        {tex}
-      </p>
+      <NavBar/>
+      
     </>
   )
 }
--- usabilidad/src/componets/navBar.jsx
+++ usabilidad/src/componets/navBar.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 
-const navBar = () => {
+const NavBar = () => {
     return (
-        <div>
-        <nav>
-            <img src="" alt="" />
+        <div className='navBar'>
+        <nav> 
+            <div className='logo'> 
+                <img src="src\assets\iconos\onlineShop.png" alt="" />
+                <h1>WEB STORE</h1>
+            </div>
+            <div className='navBar_icon'>
+                <input type="text" placeholder='Buscar'/>
+                <i class="fa-sharp fa-light fa-cart-shopping"></i>
+                <i className="fa-regular fa-user"></i>
+            </div>
         </nav>
             
         </div>
     );
 };
 
-export default navBar;
+export default NavBar;
