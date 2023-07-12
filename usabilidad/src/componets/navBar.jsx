56c5037aa80d364f51fa82e8fb337ecf95b47625
kleber chavez <kleberenriqueos@gmail.com>
2023-07-12 11:57:34 -0500

    dscf

--- usabilidad/src/componets/navBar.jsx
+++ usabilidad/src/componets/navBar.jsx
@@ -9,13 +9,12 @@ const NavBar = (login,setLogin) => {
 
     return (
         <div className='navBar'>
-<<<<<<< HEAD
+
             <div className='logo'> 
                 <img src="src\assets\iconos\onlineShop.png" href="src\main.jsx" />
-=======
+
             <div className='logo' onClick={()=>navigate(`/`)}> 
                 <img src="src\assets\iconos\onlineShop.png" />
->>>>>>> 128e75131ae98ec43d204370b09349aef0c2c4dc
                 <h1>WEB STORE</h1>
             </div>
             <div className='navBar_icon'>
@@ -25,11 +24,8 @@ const NavBar = (login,setLogin) => {
                 </div>
                 <div className='loginAndCart'>
                     <i className="fa-sharp fa-solid fa-cart-shopping"></i>
-<<<<<<< HEAD
                     <i className="fa-regular fa-user" href="src\componets\SobreNosotros.jsx"></i>
-=======
                     <i className="fa-regular fa-user" onClick={()=>navigate(`/login`)}></i>
->>>>>>> 128e75131ae98ec43d204370b09349aef0c2c4dc
                 </div>
                 
             </div>
