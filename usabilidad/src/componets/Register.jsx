62ef40118dba3360e366cca593eb25f310221b84
kleber chavez <kleberenriqueos@gmail.com>
2023-07-15 01:52:12 -0500

    creacion de hoja css registrarse

--- usabilidad/src/componets/Register.jsx
+++ usabilidad/src/componets/Register.jsx
@@ -2,13 +2,26 @@ import React from 'react';
 
 const Register = () => {
     return (
-        <div>
-            <div className='user'>
-                <input type="email" placeholder='Ingrese su email' />
-                <input type="text" placeholder='Ingrese su usuario' />
-                <input type="pasword" placeholder='Ingrese su contraseña' />
+        <div className='registrar'>
+            <div className='registrarCabesera'>
+                <img src="src\assets\iconos\onlineShop.png"/>
+                <span>REGISTARSE</span>
             </div>
-            <div>
+            <ul className='registrarList'>
+                <li>
+                    Email
+                </li>
+                <input type="email" placeholder='Ingrese su email' />
+                <li>
+                    Usuario
+                </li>
+                <input type="text" placeholder='Ingrese su usuario' />
+                <li>
+                    Contraseña
+                </li>
+                <input type="pasword" placeholder='Ingrese su contraseña' />
+            </ul>
+            <div className='registrarBoton'>
                 <button>Registrarse</button>
             </div>
         </div>
